18a0dc41327e7af297d52cb82a189f5e92bb2942
yu8ikmnbgt6y <yu8ikmnbgt6y@gmail.com>
2023-02-17 05:48:56 +0900

    works with docker-compose

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,9 @@
 export const MaxCanvasWidth = 1200;
 
-const PROTOCOL = 'http'
-const OCR_HOST = 'localhost'
-const OCR_PORT = '8080'
+const this_hostname = `${location.protocol}//${location.hostname}`
+const OCR_PORT = '8010'
 
-const OCR_URL = `${PROTOCOL}://${OCR_HOST}:${OCR_PORT}`
+const OCR_URL = `${this_hostname}:${OCR_PORT}`
 
 export const URL_OCR_API_AvailableModels = `${OCR_URL}/available_models`
 export const URL_OCR_API_AvailableLanguages = `${OCR_URL}/available_language`
